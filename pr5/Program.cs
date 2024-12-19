cafe3528b820681c419b6a13e889dc93db7337f6
yurak <yurak@DESKTOP-DMDF3TH>
2024-12-19 16:10:54 +0500

    Написан метод OnSettings. Kochetov

--- pr5/Program.cs
+++ pr5/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace pr5
 {
     public class Program
@@ -16,6 +16,7 @@ namespace pr5
         static List<Client> AllClients = new List<Client>();
         static void Main(string[] args)
         {
+            OnSettings();
         }
         static void Help()
         {
@@ -33,6 +34,41 @@ namespace pr5
             Console.Write("/status");
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("  - show list users");
+        }
+        static void OnSettings()
+        {
+            string Path = Directory.GetCurrentDirectory() + "/.config";
+            if (File.Exists(Path))
+            {
+                StreamReader sr = new StreamReader(Path);
+                ServerIPAddress = IPAddress.Parse(sr.ReadLine());
+                ServerPort = int.Parse(sr.ReadLine());
+                MaxClient = int.Parse(sr.ReadLine());
+                Duration = int.Parse(sr.ReadLine());
+                sr.Close();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Server IP-address: {ServerIPAddress.ToString()};\nServer port: {ServerPort};\nMax client: {MaxClient};\nDuration: {Duration};");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write($"Please, provide the IP-address: ");
+                ServerIPAddress = IPAddress.Parse(Console.ReadLine());
+                Console.Write($"Please, specify the port: ");
+                ServerPort = int.Parse(Console.ReadLine());
+                Console.Write($"Please, specify the maximum number of clients: ");
+                MaxClient = int.Parse(Console.ReadLine());
+                Console.Write($"Please, specify the duration of the license: ");
+                Duration = int.Parse(Console.ReadLine());
+                StreamWriter sw = new StreamWriter(Path);
+                sw.WriteLine(ServerIPAddress.ToString());
+                sw.WriteLine(ServerPort);
+                sw.WriteLine(MaxClient);
+                sw.WriteLine(Duration);
+                sw.Close();
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("To change, write the command: /config");
         }
     }
 }
